31a39707804936870174392743f4daeac5d8a1b2
Mannulus <pipe_050401@hotmail.com>
2025-08-13 00:05:13 -0500

    feat: add cors origin

---
+++ src/main/java/org/bipal/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.bipal;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@Profile("prod") para cuando se configure en producción
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
